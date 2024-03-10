4285f55e99c5606a7bc7e0f1bd0f7abb20b5f0c1
Volodymyr Komarov <vkomarov01@mynbcc.ca>
2024-03-10 12:30:41 -0300

    Read more animation

--- JS/main.js
+++ JS/main.js
@@ -16,19 +16,34 @@ $(document).ready(function () {
 function readMore(button) {
   // We use the button's parent node to scope our search for related elements
   var container = button.parentNode;
-
   // Find the dots and moreText within the container
   var dots = container.querySelector('.dots');
   var moreText = container.querySelector('.more');
 
-  // Toggle the dots and moreText display style
+  // // Toggle the dots and moreText display style
+  // if (dots.style.display === 'none') {
+  //   dots.style.display = 'inline';
+  //   button.textContent = 'Read More'; // Update the button text
+  //   moreText.style.display = 'none';
+  // } else {
+  //   dots.style.display = 'none';
+  //   button.textContent = 'Read Less'; // Update the button text
+  //   moreText.style.display = 'inline';
+  // }
+
   if (dots.style.display === 'none') {
     dots.style.display = 'inline';
-    button.textContent = 'Read More'; // Update the button text
-    moreText.style.display = 'none';
+    button.textContent = 'Read More';
+    moreText.style.opacity = 0;
+    setTimeout(function () {
+      moreText.style.display = 'none';
+    }, 300); // Match the duration of the transition
   } else {
     dots.style.display = 'none';
-    button.textContent = 'Read Less'; // Update the button text
+    button.textContent = 'Read Less';
     moreText.style.display = 'inline';
+    setTimeout(function () {
+      moreText.style.opacity = 1;
+    }, 10); // Give it a moment to apply display style
   }
 }
